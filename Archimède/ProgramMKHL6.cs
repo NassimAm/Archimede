0caf5cfce31efc3f9253257f91a52e3bd797f27b
Marouane <MarouaneBenbetka@gmail.com>
2022-05-01 18:14:15 +0100

    clean program.cs

---
+++ Archimède/ProgramMKHL6.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Archimède
+{
+    internal class ProgramMKHL6
+    {
+    }
+}
